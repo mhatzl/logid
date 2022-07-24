57e00baef8b9d32c6b9dc00e67d02439b64b0a89
Manuel Hatzl <hatzlmanuel@outlook.com>
2022-07-24 16:08:48 +0200

    fix: add `use` declaration to usage example

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 //! Library providing functionalities to set and capture [`log_id::LogId`]s.
-//! 
+//!
 //! **Usage:**
-//! 
+//!
 //! ~~~
+//! use logid::{log_id::{LogId, EventLevel, get_log_id}, capturing::LogIdTracing};
+//!
 //! const SOME_ERROR: LogId = get_log_id(0, 0, EventLevel::Error, 0);
-//! 
+//!
 //! fn my_func() -> Result<usize, LogId> {
 //!   // some code ...
-//! 
+//!
 //!   // on error
 //!   Err(SOME_ERROR.set_event("Some error message", file!(), line!())
 //!       .add_cause("Cause of error -> unknown").finalize()  
