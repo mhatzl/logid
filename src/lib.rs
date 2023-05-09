2a62c098b78f919ac3f520fb73f82694baf58b1b
Manuel Hatzl <hatzlmanuel@outlook.com>
2023-05-09 15:22:15 +0200

    doc: improve lib.rs documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-//! Library providing functionalities to set and capture [`log_id::LogId`]s.
+//! Library providing functionalities to set and capture [`log_id::LogId`] events.
+//! [`log_id::LogId`]s are used to identify and group events.
+//! An event is captured by a central map by explicitly using [`finalize()`](capturing::LogIdEvent::finalize()),
+//! converting the event into the [`log_id::LogId`] used to set the event,
+//! or implicitly on `drop` of the event.
+//!
+//! To access captured events, checkout the functions and macros provided by the [`id_map`] module.
 //!
 //! **Usage:**
 //!
