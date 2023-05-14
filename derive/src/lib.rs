bcb35a619807e07cc4d3e2c19fbd338865075f78
Manuel Hatzl <hatzlmanuel@outlook.com>
2023-05-14 18:45:05 +0200

    feat: add new LogId struct and enum error derive

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -2,39 +2,110 @@ use proc_macro::TokenStream;
 use quote::quote_spanned;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(FromLogId)]
-pub fn derive_from_log_id(input: TokenStream) -> TokenStream {
+// #[proc_macro_derive(FromLogId)]
+// pub fn derive_from_log_id(input: TokenStream) -> TokenStream {
+//     let input = parse_macro_input!(input as DeriveInput);
+//     let enum_name = input.ident;
+
+//     match input.data {
+//         syn::Data::Enum(enum_data) => {
+//             let span = enum_name.span();
+//             let mut fields = Vec::new();
+
+//             for variant in enum_data.variants {
+//                 let field_name = variant.ident;
+//                 let full_field_name = quote_spanned! {span=>
+//                     #enum_name :: #field_name
+//                 };
+//                 fields.push(quote_spanned! {span=>
+//                     v if v == logid::logid!(#full_field_name) => #full_field_name,
+//                 });
+//             }
+
+//             let expanded = quote_spanned! {span=>
+//                 impl From<logid::log_id::LogId> for #enum_name {
+//                     fn from(value: logid::log_id::LogId) -> Self {
+//                         match value {
+//                             #(#fields)*
+//                             _ => Self::default(),
+//                         }
+//                     }
+//                 }
+//             };
+
+//             TokenStream::from(expanded)
+//         }
+//         _ => panic!("Derive `FromLogId` is only implemented for enumerations."),
+//     }
+// }
+
+#[proc_macro_derive(ErrLogId)]
+pub fn derive_err_log_id(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let enum_name = input.ident;
 
     match input.data {
         syn::Data::Enum(enum_data) => {
             let span = enum_name.span();
-            let mut fields = Vec::new();
+            let mut from_fields = Vec::new();
+            let mut field_identifiers = Vec::new();
 
             for variant in enum_data.variants {
                 let field_name = variant.ident;
                 let full_field_name = quote_spanned! {span=>
-                    #enum_name :: #field_name
+                    #enum_name::#field_name
                 };
-                fields.push(quote_spanned! {span=>
-                    v if v == logid::logid!(#full_field_name) => #full_field_name,
+                field_identifiers.push(quote_spanned! {span=>
+                    #full_field_name => "#full_field_name",
+                });
+                from_fields.push(quote_spanned! {span=>
+                    v if v == "#full_field_name" => #full_field_name,
                 });
             }
 
-            let expanded = quote_spanned! {span=>
+            let from_event_id = quote_spanned! {span=>
                 impl From<logid::log_id::LogId> for #enum_name {
                     fn from(value: logid::log_id::LogId) -> Self {
-                        match value {
-                            #(#fields)*
+                        if value.get_log_level() != logid::log_id::LogLevel::Error
+                            || value.get_crate_name() != env!("CARGO_PKG_NAME")
+                            || value.get_module_path() != module_path!() {
+
+                            return Self::default();
+                        }
+
+                        match value.get_identifier() {
+                            #(#from_fields)*
                             _ => Self::default(),
                         }
                     }
                 }
             };
 
-            TokenStream::from(expanded)
+            let from_enum = quote_spanned! {span=>
+                impl From<#enum_name> for logid::log_id::LogId {
+                    fn from(value: #enum_name) -> Self {
+                        let field_name = match value {
+                            #(#field_identifiers)*
+                        };
+
+                        logid::log_id::LogId::new(
+                            env!("CARGO_PKG_NAME"),
+                            module_path!(),
+                            field_name,
+                            logid::log_id::LogLevel::Error,
+                        )
+                    }
+                }
+            };
+
+            let froms = quote_spanned! {span=>
+                #from_event_id
+
+                #from_enum
+            };
+
+            TokenStream::from(froms)
         }
-        _ => panic!("Derive `FromLogId` is only implemented for enumerations."),
+        _ => panic!("Derive `ErrLogId` is only implemented for enumerations."),
     }
 }
--- derive/tests/from_log_id.rs
+++ derive/tests/from_log_id.rs
@@ -1,60 +1,60 @@
-use logid::{
-    log_id::{get_log_id, LogLevel},
-    logid,
-};
-use logid_derive::FromLogId;
+// use logid::{
+//     log_id::{get_log_id, LogLevel},
+//     logid,
+// };
+// use logid_derive::FromLogId;
 
-#[derive(PartialEq, Eq, Debug, Default, FromLogId)]
-enum LogIdEnum {
-    #[default]
-    First = get_log_id(0, 0, LogLevel::Debug, 1),
-    Second = get_log_id(0, 0, LogLevel::Debug, 2),
-    Third = get_log_id(0, 0, LogLevel::Debug, 3),
-}
+// #[derive(PartialEq, Eq, Debug, Default, FromLogId)]
+// enum LogIdEnum {
+//     #[default]
+//     First = get_log_id(0, 0, LogLevel::Debug, 1),
+//     Second = get_log_id(0, 0, LogLevel::Debug, 2),
+//     Third = get_log_id(0, 0, LogLevel::Debug, 3),
+// }
 
-#[derive(PartialEq, Eq, Debug, Default, FromLogId)]
-enum OtherLogIdEnum {
-    One = get_log_id(0, 0, LogLevel::Warn, 1),
-    Two = get_log_id(0, 0, LogLevel::Warn, 2),
-    #[default]
-    Three = get_log_id(0, 0, LogLevel::Warn, 3),
-}
+// #[derive(PartialEq, Eq, Debug, Default, FromLogId)]
+// enum OtherLogIdEnum {
+//     One = get_log_id(0, 0, LogLevel::Warn, 1),
+//     Two = get_log_id(0, 0, LogLevel::Warn, 2),
+//     #[default]
+//     Three = get_log_id(0, 0, LogLevel::Warn, 3),
+// }
 
-#[test]
-fn enum_roundtrip_conversion() {
-    let first_logid = logid!(LogIdEnum::First);
-    let second_logid = logid!(LogIdEnum::Second);
-    let third_logid = logid!(LogIdEnum::Third);
+// #[test]
+// fn enum_roundtrip_conversion() {
+//     let first_logid = logid!(LogIdEnum::First);
+//     let second_logid = logid!(LogIdEnum::Second);
+//     let third_logid = logid!(LogIdEnum::Third);
 
-    assert_eq!(
-        LogIdEnum::from(first_logid),
-        LogIdEnum::First,
-        "Wrong roundtrip conversion to first enum."
-    );
-    assert_eq!(
-        LogIdEnum::from(second_logid),
-        LogIdEnum::Second,
-        "Wrong roundtrip conversion to second enum."
-    );
-    assert_eq!(
-        LogIdEnum::from(third_logid),
-        LogIdEnum::Third,
-        "Wrong roundtrip conversion to third enum."
-    );
+//     assert_eq!(
+//         LogIdEnum::from(first_logid),
+//         LogIdEnum::First,
+//         "Wrong roundtrip conversion to first enum."
+//     );
+//     assert_eq!(
+//         LogIdEnum::from(second_logid),
+//         LogIdEnum::Second,
+//         "Wrong roundtrip conversion to second enum."
+//     );
+//     assert_eq!(
+//         LogIdEnum::from(third_logid),
+//         LogIdEnum::Third,
+//         "Wrong roundtrip conversion to third enum."
+//     );
 
-    assert_eq!(
-        std::convert::Into::<LogIdEnum>::into(first_logid),
-        LogIdEnum::First,
-        "Wrong conversion to first enum."
-    );
-    assert_eq!(
-        std::convert::Into::<LogIdEnum>::into(second_logid),
-        LogIdEnum::Second,
-        "Wrong conversion to second enum."
-    );
-    assert_eq!(
-        std::convert::Into::<LogIdEnum>::into(third_logid),
-        LogIdEnum::Third,
-        "Wrong conversion to third enum."
-    );
-}
+//     assert_eq!(
+//         std::convert::Into::<LogIdEnum>::into(first_logid),
+//         LogIdEnum::First,
+//         "Wrong conversion to first enum."
+//     );
+//     assert_eq!(
+//         std::convert::Into::<LogIdEnum>::into(second_logid),
+//         LogIdEnum::Second,
+//         "Wrong conversion to second enum."
+//     );
+//     assert_eq!(
+//         std::convert::Into::<LogIdEnum>::into(third_logid),
+//         LogIdEnum::Third,
+//         "Wrong conversion to third enum."
+//     );
+// }
