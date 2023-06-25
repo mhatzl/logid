a5c90b3598ea3b0ee796e42f3f4902beb188312f
Manuel Hatzl <hatzlmanuel@outlook.com>
2023-06-25 21:19:38 +0200

    feat: add more advanced benchmark

--- bench/benches/bench.rs
+++ bench/benches/bench.rs
@@ -1,5 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use logid::{event_handler::LogEventHandlerBuilder, log, log_id::LogLevel};
+use logid::{
+    err, event_handler::LogEventHandlerBuilder, log, log_id::LogLevel,
+    logging::event_entry::AddonKind, DbgLogId, ErrLogId, InfoLogId, WarnLogId,
+};
+
+criterion_main!(benches);
+criterion_group!(benches, bench_compare_advanced_logging);
+// criterion_group!(benches, bench_error_tracing, bench_error_logid);
 
 pub fn bench_error_tracing(c: &mut Criterion) {
     tracing_subscriber::fmt::init();
@@ -49,7 +56,71 @@ pub fn bench_compare(c: &mut Criterion) {
     log_handler.shutdown();
 }
 
-criterion_group!(benches, bench_compare);
-// criterion_group!(benches, bench_error_tracing, bench_error_logid);
+pub fn bench_compare_advanced_logging(c: &mut Criterion) {
+    tracing_subscriber::fmt::init();
 
-criterion_main!(benches);
+    let _ = logid::set_filter!("debug(infos)");
+
+    let log_handler = LogEventHandlerBuilder::new()
+        .to_stderr()
+        .all_log_events()
+        .build();
+
+    let mut bench_group = c.benchmark_group("compare advanced bench");
+
+    bench_group.bench_function("tracing", |b| b.iter(|| advanced_tracing()));
+
+    bench_group.bench_function("logid", |b| b.iter(|| advanced_logid()));
+
+    bench_group.finish();
+
+    log_handler.shutdown();
+}
+
+fn advanced_logid() -> Result<(), BenchError> {
+    log!(BenchError::Test, black_box("Logid error in advanced bench."), add: AddonKind::Info(black_box("Trace info in advanced bench related to error trace.").to_string()));
+
+    log!(BenchWarn::Test, black_box("Logid warn in advanced bench."));
+    log!(BenchInfo::Test, black_box("Logid info in advanced bench."));
+    log!(BenchDbg::Test, black_box("Logid debug in advanced bench."));
+
+    err!(BenchError::Test)
+}
+
+fn advanced_tracing() -> Result<(), BenchError> {
+    tracing::error!("{}", black_box("Trace error in advanced bench."));
+    tracing::info!(
+        "{}",
+        black_box("|--> Info: Trace info in advanced bench related to error trace.")
+    );
+
+    tracing::warn!("{}", black_box("Trace warn in advanced bench."));
+    tracing::info!("{}", black_box("Trace info in advanced bench."));
+    tracing::debug!("{}", black_box("Trace debug in advanced bench."));
+
+    Err({
+        tracing::error!("{}", black_box(BenchError::Test.to_string()));
+        BenchError::Test
+    })
+}
+
+#[derive(Debug, Clone, thiserror::Error, ErrLogId)]
+enum BenchError {
+    #[error("Some benchmark test error")]
+    Test,
+}
+
+#[derive(Debug, Clone, WarnLogId)]
+enum BenchWarn {
+    Test,
+}
+
+#[derive(Debug, Clone, InfoLogId)]
+enum BenchInfo {
+    Test,
+}
+
+#[derive(Debug, Clone, DbgLogId)]
+enum BenchDbg {
+    Test,
+}
